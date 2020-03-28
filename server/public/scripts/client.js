66d8054ab870b2f8b78f72268b6c302ac3dc55cd
Caleb Wistuba <calebwistuba@gmail.com>
2020-03-28 15:38:47 -0500

    operator btn and equal btn working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,38 @@
+let operator;
+
 $(document).ready(init);
 
 function init() {
   console.log("DOM is ready");
 
   //   getCalculations();
+  $(".js-btn-equals").on("click", submitInput);
+  $(".js-btn-mathOperator").on("click", function(event) {
+    operator = event.target.innerHTML;
+    console.log(operator);
+    //  let oper = event.target.innerHTML;
+    //  handleOperator(oper)
+  });
 }
 
 //
 //EVENT HANDLERS
 // -------------
+
+// function handleOperator(oper){
+//     operator = oper;
+// }
+
+function submitInput() {
+  console.log("clicked!");
+
+  // const newItem = {
+  //   num1: $(".js-input-firstNumber").val(),
+  //   num2: $(".js-input-secondNumber").val(),
+  //   operator: operator,
+  //   output: ""
+  // };
+}
 
 //
 // API INTERACTIONS / AJAX CALLS
