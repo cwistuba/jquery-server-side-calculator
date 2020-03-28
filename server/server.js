5a17c8412a9880f0741e16499c50e31d3e38f4e9
Caleb Wistuba <calebwistuba@gmail.com>
2020-03-28 12:01:52 -0500

    creating a calculations inventory route to return all calculations

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const calculation = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/calculation", (req, res) => {
+  res.send(calculation);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
